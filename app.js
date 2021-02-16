edca84730465ccd59ee6a091a6538cca0aeed802
Andrew <aaoyanguren@outlook.com>
2021-02-15 18:52:04 -0800

    Completed HTML, CSS, and JavaScript

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
 // ===== Variables ===== //
 
+const textEl = document.getElementById('text');
+const speedEl = document.getElementById('speed');
+const text = 'We Love Programming';
 
+let idx = 1;
+let speed = 300 / speedEl.value;
 
 // ===== Event Listeners ===== //
 
-
+speedEl.addEventListener('input', (e) => speed = 300 / e.target.value);
 
 // ===== Functions ===== //
+
+writeText();
+
+function writeText() {
+  textEl.innerText = text.slice(0, idx)
+  // start incremention
+  idx++
+  // check if end of string
+  if (idx > text.length) {
+    idx = 1
+  }
+  // add speed element to our funtionality
+  setTimeout(writeText, speed);
+}
